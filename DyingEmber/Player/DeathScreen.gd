d4b2107e894d079fe44c5d61fd98ed9164a8cac6
Michael-Sjogren <carl.michael.sjogren@gmail.com>
2020-04-20 20:31:24 +0200

    fixed fonts

--- DyingEmber/Player/DeathScreen.gd
+++ DyingEmber/Player/DeathScreen.gd
@@ -1,13 +1,16 @@
 extends CanvasLayer
 
 onready var rect = $Control/ColorRect
+onready var control = $Control
 
 func _ready():
+	control.hide()
 	rect.hide()
 	get_parent().connect("player_died",self,"_on_Player_death")
 	
 
 func _on_Player_death():
 	print("player died")
+	control.show()
 	rect.show()
 	
